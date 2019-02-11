cd8ad81123f93f0c59c002c3006a2cf3d0ad4960
Romain Ragonnet <romain.ragonnet@burnet.edu.au>
2019-02-11 17:02:55 +1100

    Create second objective function to minimise

--- nogui_run.py
+++ nogui_run.py
@@ -1,8 +1,12 @@
 import autumn.model_runner
 import autumn.outputs as outputs
+import autumn.model as model
+import autumn.tool_kit as t_k
 from copy import copy
 
 ''' some preliminary work '''
+
+
 def run_model(characteristics):
     """
     This method runs the model based on the parameters listed in 'characteristics'.
@@ -52,86 +56,125 @@ standard_gui_outputs = {'is_shortcourse_improves_outcomes': True, 'pickle_uncert
                'plot_option_plot_all_vars': False}
 
 ''' define several versions of the simulator '''
+# baseline version
 characteristics_1 = copy(standard_gui_outputs)
 
 # refining time-step
 characteristics_2 = copy(standard_gui_outputs)
 characteristics_2['time_step'] = 0.1 # baseline was 0.5
 
-# change integration method
+# changing integration method
 characteristics_3 = copy(standard_gui_outputs)
 characteristics_3['integration_method'] = 'Runge Kutta'  # baseline was Explicit
 
-# change age brackets
+# changing age brackets
 characteristics_4 = copy(standard_gui_outputs)
 characteristics_4['age_breakpoints'] = [5, 15, 25, 35, 45]   # baseline was [5, 15, 25]
 
-# include more risk-groups (further stratify the population)
+# including more risk-groups (further stratifying the population)
 characteristics_5 = copy(standard_gui_outputs)
 characteristics_5['riskgroup_prison'] = True  # baseline was False
 characteristics_5['riskgroup_hiv'] = True  # baseline was False
 characteristics_5['riskgroup_ruralpoor'] = True  # baseline was False
 characteristics_5['riskgroup_urbanpoor'] = True  # baseline was False
 
-''' define objective functions associated with different optimisation problems '''
-annual_budget = 1.e6  # total budget available every year
-tb_indicator = "incidence"   # could be "mortality" or "prevalence" or "notifications"
 
+''' define two objective functions associated with different optimisation problems '''
+# First, we need to run a baseline model for initialisation
 baseline_m_r = run_model(characteristics_1)
-baseline_m_r.inputs.run_mode = 'spending_inputs'
+
+objective_option = 2  # 1 to optimise the funding allocation / 2 to calibrate the model
+
+if objective_option == 1:
+    annual_budget = 1.e6  # total budget available every year
+    tb_indicator = "incidence"   # could be "mortality" or "prevalence" or "notifications"
+    baseline_m_r.inputs.run_mode = 'spending_inputs'
+
+    def objective_1(funding):
+        """
+        Objective function associated with the problem of resource allocation where there is a constraint on the budget
+        Note that this function requires a baseline model to be run first.
+        Args:
+            funding: dictionary keyed with the names of the different interventions and valued with the allocated funding
+            proportion. This proportion represents the share of the annual budget allocated for each intervention.
+
+        Returns:
+            the estimated incidence (or mortality, or prevalence, or notification) of tuberculosis in 2035
+        """
+        # Check that the constraint is verified
+        assert sum(funding.values()) <= 1., "The sum of the funding proportions should be <= 1"
+
+        # Create a spending_plan dictionary formatted to match model requirements
+        spending_plan = {}
+        for key, val in funding.iteritems():
+            spending_plan[key] = {}
+            for year in range(
+                            int(baseline_m_r.inputs.original_data['default_constants']['before_intervention_time']) - 1,
+                            int(baseline_m_r.inputs.original_data['default_constants']['scenario_end_time']) + 1):
+                spending_plan[key][year] = val * annual_budget
+
+        # run model from 2014 until 2035 with funding driving the epidemic
+        baseline_m_r.run_spending_inputs(spending_plan)
+        # read the output of interest
+        outcome = baseline_m_r.outputs['manual']['epi'][17][tb_indicator][-1]
+        return outcome
+
+    # example parameter set
+    funding_test = {'int_perc_ipt_age0to5': 0.2,
+                    'int_perc_ipt_age5to15': 0.1,
+                    'int_perc_xpertacf': 0.2,
+                    'int_perc_decentralisation': 0.2,
+                    'int_perc_treatment_support_relative': 0.2,
+                    'int_perc_xpert': 0.1
+                    }
+    objective = objective_1(funding_test)
+elif objective_option == 2:
 
 
-def objective_1(funding):
-    """
-    Objective function associated with the problem of resource allocation where there is a constraint on the budget
-    Note that this function requires a baseline model to be run first.
-    Args:
-        funding: dictionary keyed with the names of the different interventions and valued with the allocated funding
-        proportion. This proportion represents the share of the annual budget allocated for each intervention.
+    def objective_2(parameters):
+        """
+        Objective function associated with a problem of model calibration. This tries to match the simulated TB incidence
+        with official estimates from the World Health Organization (WHO) reported for years 2010-2015.
+        Args:
+            parameters: a set of parameters that we aim to calibrate. They are listed in a dictionary
 
-    Returns:
-        the estimated incidence (or mortality, or prevalence, or notification) of tuberculosis in 2035
-    """
-    # Check that the constraint is verified
-    assert sum(funding.values()) <= 1., "The sum of the funding proportions should be <= 1"
+        Returns: a "squared distance" between the model predictions and the WHO estimates
+        """
+        baseline_m_r.inputs.outputs_unc = [{'posterior_width': None, 'key': 'incidence', 'width_multiplier': 1.0}]
+        baseline_m_r.inputs.get_data_to_fit()
+        working_output_dictionary = baseline_m_r.get_fitting_data('incidence')
+        requested_years = baseline_m_r.requested_years
+        available_years = [y for y in requested_years if y in working_output_dictionary.keys()]
+        weights = [1. / float(len(available_years))] * len(available_years)
+        target_values = [working_output_dictionary[y][0] for y in available_years]
 
-    # Create a spending_plan dictionary formatted to match model requirements
-    spending_plan = {}
-    for key, val in funding.iteritems():
-        spending_plan[key] = {}
-        for year in range(int(baseline_m_r.inputs.original_data['default_constants']['before_intervention_time']) - 1,
-                                  int(baseline_m_r.inputs.original_data['default_constants']['scenario_end_time']) + 1):
-            spending_plan[key][year] = val * annual_budget
+        baseline_m_r.models[0] = model.ConsolidatedModel(0, baseline_m_r.inputs, baseline_m_r.gui_inputs)
+        baseline_m_r.set_model_with_params(parameters, 0)
+        baseline_m_r.models[0].integrate()
+        baseline_m_r.outputs['manual'] = {'epi': {}, 'cost': {}}
+        baseline_m_r.outputs['manual']['epi'][0] \
+            = baseline_m_r.find_epi_outputs(0, strata_to_analyse=[baseline_m_r.models[0].agegroups, baseline_m_r.models[0].riskgroups])
+        outputs_for_comparison \
+            = [baseline_m_r.outputs['manual']['epi'][0]['incidence'][
+                   t_k.find_first_list_element_at_least(baseline_m_r.outputs['manual']['epi'][0]['times'], float(year))]
+               for year in available_years]
 
-    # run model from 2014 until 2035 with funding driving the epidemic
-    baseline_m_r.run_spending_inputs(spending_plan)
-    # read the output of interest
-    outcome = baseline_m_r.outputs['manual']['epi'][17][tb_indicator][-1]
-    return outcome
+        # find distance result for run
+        sum_of_squares, index_for_available_years = 0., 0
+        for year in range(len(available_years)):
+            sum_of_squares += weights[year] * (target_values[year] - outputs_for_comparison[year]) ** 2
 
+        return sum_of_squares
 
-def objective_2(parameters):
-    """
+    # example parameter set
+    parameters_test = {'tb_n_contact': 10.,
+                  'tb_multiplier_treated_protection': 0.6,
+                  'tb_prop_casefatality_untreated_smearpos': 0.7,
+                  'tb_timeperiod_activeuntreated': 2.8,
+                  }
 
-    Args:
-        parameters:
+    objective = objective_2(parameters_test)
+else:
+    print "Objective option not recognised"
 
-    Returns:
-
-    """
-    pass
-
-
-funding = {'int_perc_ipt_age0to5': 0.2,
-            'int_perc_ipt_age5to15': 0.1,
-           'int_perc_xpertacf': 0.2,
-           'int_perc_decentralisation': 0.2,
-           'int_perc_treatment_support_relative': 0.2,
-           'int_perc_xpert': 0.1
-           }
-
-parameters = {'tb_n_contact': 10.,
-              'tb_multiplier_treated_protection': 0.6,
-              'tb_prop_casefatality_untreated_smearpos': 0.7,
-              'tb_timeperiod_activeuntreated': 2.8,
-              }
+print objective
